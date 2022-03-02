9744a2d6f3b2efb868cfd43230e55a5953fa1ab9
Makar <ne_ya@me.com>
2022-03-02 15:16:00 +0300

    master

--- deploy.js
+++ deploy.js
@@ -1,21 +1,25 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const {interface, bytecode} = require('./compile');
 
-const provider = new HDWalletProvider(
-'shadow dutch satisfy enemy above clean army purse vendor purity patrol frost',
-'https://rinkeby.infura.io/v3/d82e9d2de57042fbb42a4ac0788c179c'
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'REPLACE_WITH_YOUR_MNEMONIC',
+  'REPLACE_WITH_YOUR_INFURA_URL'
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts();
-    console.log('attempting to deploy from acc', accounts[0]);
+  const accounts = await web3.eth.getAccounts();
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data : bytecode, arguments: ['hi there!']})
-        .send({gas: 1000000, from : accounts[0]});
-    console.log('Contract deployed to ', result.options.address);
-    provider.engine.stop();
-    };
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
 deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,66 +1,35 @@
 const assert = require('assert');
-const ganache =  require('ganache-cli');
+const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const {interface, bytecode} = require('../compile')
+
+const { abi, evm } = require('../compile');
 
 let accounts;
 let inbox;
-// let INITIAL_STRING = 'Hi there!'
 
-beforeEach(async () =>{
-    //geting list of all acc
-    accounts = await web3.eth.getAccounts();
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({
-            data: bytecode, 
-            arguments:['Hi there!'],
-        })
-        .send({from: accounts[0], gas:1000000})
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: ['Hi there!'],
+    })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
-    it("deploys a contract", () => {
-        assert.ok(inbox.options.address);
-    });
-    it("has a default message", async () => {
-        const message = await inbox.methods.message().call();
-        assert.equal(message, 'Hi there!');
-    });
-    it('can change the message', async () => {
-        await inbox.methods.setMessage('bye').send({from: accounts[0], gas:1000000});
-        const message = await inbox.methods.message().call();
-        assert.equal(message,"bye");
-    });
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+  });
 });
-
-
-//trying mocha
-/*class Car{
-    park(){
-        return 'stopped';
-    }
-
-    drive(){
-        return 'vroom';
-    }
-}
-
-let car;
-
-beforeEach(()=>{
-    car = new Car();
-});
-
-describe('Car', ()=> {
-    it('can park', ()=>{
-        const car = new Car();
-        assert.equal(car.park(), 'stopped');
-    });
-
-    it('can park',()=>{
-        const car = new Car();
-        assert.equal(car.drive(), 'vroom')
-    });
-
-});*/
